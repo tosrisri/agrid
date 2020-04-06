3534ef92ed5714c2201514b724c24a6ce49cf4af
sbuddha <sbuddha@ziperase.com>
2020-04-06 13:28:27 +0530

    First commit with basic ag-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agrid';
+
+  columnDefs = [
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" }
+  ];
+  rowData = [
+    { make: "Toyota", model: "Celica", price: "32000" },
+    { make: "Ford", model: "Mondeo", price: "29000" },
+    { make: "Porche", model: "Boxter", price: "45000" }
+  ];
 }
