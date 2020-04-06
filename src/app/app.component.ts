95d8a02719215651280bc0424c9bd5e495209b1b
sbuddha <sbuddha@ziperase.com>
2020-04-06 15:27:26 +0530

    Implemented Sorting, Filtering, API instead of static data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient  } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,23 @@ export class AppComponent {
   title = 'agrid';
 
   columnDefs = [
-    { headerName: "Make", field: "make" },
-    { headerName: "Model", field: "model" },
-    { headerName: "Price", field: "price" }
+    { headerName: "Name", field: "name"  , sortable: true, filter:true, checkboxSelection: true },
+    { headerName: "User Name", field: "username", sortable: true, filter:true },
+    { headerName: "Email", field: "email", sortable: true, filter:true }
   ];
-  rowData = [
-    { make: "Toyota", model: "Celica", price: "32000" },
-    { make: "Ford", model: "Mondeo", price: "29000" },
-    { make: "Porche", model: "Boxter", price: "45000" }
-  ];
+  rowData: any;
+
+constructor(private http: HttpClient) {
+
+}
+
+ngOnInit(){
+  this.rowData = this.http.get("https://jsonplaceholder.typicode.com/users");
+}
+
+  // rowData = [
+  //   { name: "Toyota", username: "Celica", email: "tc@y.com" },
+  //   { name: "Ford", username: "Mondeo", email: "bm@g.com" },
+  //   { name: "Porche", username: "Boxter", email: "nu@s.com" }
+  // ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AgGridModule } from 'ag-grid-angular'
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AgGridModule.withComponents([])
+    BrowserModule,
+    HttpClientModule, 
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
